86396d49d352b8f752ca3ca0eb006869025c1ad9
Chris Elliott <christopher7elliott@gmail.com>
2018-08-09 12:42:22 -0700

    List of changes: Fixed 'questions' in each event page (and the corresponding bottom margin), fixed the 'sponsor' link on each page (goes to homepage now), fixed the Eunbin image size, and separated the Kernel Labs and Seattle events (the dates and information were getting 'mishmashed'.

---
+++ _site/projects.js
@@ -0,0 +1,23 @@
+/*
+var projects = [];
+
+p1 = {
+  header: "IMRC",
+  title: "Web Developer (September 2017 - Present)",
+  description: "Design, develop and maintain the websites of Lehigh’s College of Arts and Sciences. Partner with photographers and graphic designers. Incorporates feedback from faculty and IMRC teammates to improve website functionality."
+  chips: [0, 1];
+}
+
+projects.push(p1);
+
+p2 = {
+  header: "Soterra",
+  title: "Software Developer, Researcher (September 2017 - Present)",
+  description: "Research, program and engineer a device under $40 that helps women in developing countries report sexual harrasment in less than 90 seconds."
+  chips: [0, 1];
+}
+
+projects.push(p2);
+*/
+
+alert("HELLO");
