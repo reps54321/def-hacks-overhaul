c46a68428896d3c383fd393cc93a1241d450fd8e
Chris Elliott <christopher7elliott@gmail.com>
2018-09-05 13:19:03 -0700

    new sponsorship prospectus

---
+++ _site/tipuesearch/tipuesearch_content.js
@@ -0,0 +1,7 @@
+
+var tipuesearch = {"pages": [
+     {"title": "Transportation", "text": "", "tags": "transportation cars drivers driving ", "url": "transportation.html"},
+     {"title": "Cashier", "text": "Cashier (in Grocery Stores, Department Stores and \"Other General Merchandise\" Stores) has been put on yellow alert.", "tags": "buying purchasing stores store clerk shop shopping cahsier", "url": "cashier.html"},
+     {"title": "Medical Diagnosing", "text": "Health Diagnosing has been put on watch.", "tags": "health doctor nurse diagnosis diagnosing", "url": "diagnosis.html"},
+     {"title": "Lawyering", "text": "Lawyering (Discovery) has been put on watch.", "tags": "lawyer files reading discovery", "url": "lawyering-discovery0.html"},
+]};
