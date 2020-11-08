2509dd3b193c5bebcbb75ebf5a7c6b9cbd55629d
deepak <dk721572@gmail.com>
2020-11-08 21:18:55 +0530

    added schedule

--- js/global_2.0.js
+++ js/global_2.0.js
@@ -7,10 +7,12 @@
   const aboutSec = document.querySelector('#About')
   const speakersSec = document.querySelector('#Speakers')
   const sponsorsSec = document.querySelector('#Sponsors')
+  const scheduleSec = document.querySelector('#Schedule')
   const FAQSec = document.querySelector('#FAQ')
   const topOfAbout = aboutSec.offsetTop  + 706
   const topOfSponsors = sponsorsSec.offsetTop + 706
   const topOfSpeakers = speakersSec.offsetTop + 706
+  const topOfSchedule = scheduleSec.offsetTop + 706
   const topOfFAQ = FAQSec.offsetTop + 706
 
   function fixNav() {
@@ -30,26 +32,39 @@
         document.querySelector('#Spk').classList.remove('selected')
         document.querySelector('#Spo').classList.remove('selected')
         document.querySelector('#Fq').classList.remove('selected')
+        document.querySelector('#Sch').classList.remove('selected')
         break;
       case (window.scrollY > topOfSpeakers && window.scrollY <= topOfSponsors):
         document.querySelector('#Spk').classList.add('selected')
+        document.querySelector('#Sch').classList.remove('selected')
         document.querySelector('#Abt').classList.remove('selected')
         document.querySelector('#Spo').classList.remove('selected')
         document.querySelector('#Fq').classList.remove('selected')
         break;
-      case (window.scrollY > topOfSponsors && window.scrollY <= topOfFAQ):
+      case (window.scrollY > topOfSponsors && window.scrollY <= topOfSchedule):
         document.querySelector('#Spo').classList.add('selected')
+        document.querySelector('#Sch').classList.remove('selected')
         document.querySelector('#Spk').classList.remove('selected')
         document.querySelector('#Abt').classList.remove('selected')
         document.querySelector('#Fq').classList.remove('selected')
         break;
+      case (window.scrollY > topOfSchedule && window.scrollY <= topOfFAQ):
+        document.querySelector('#Sch').classList.add('selected')
+        document.querySelector('#Fq').classList.remove('selected')
+        document.querySelector('#Spo').classList.remove('selected')
+        document.querySelector('#Spk').classList.remove('selected')
+        document.querySelector('#Abt').classList.remove('selected')
+        break;
       case (window.scrollY > topOfFAQ):
         document.querySelector('#Fq').classList.add('selected')
+        document.querySelector('#Sch').classList.remove('selected')
         document.querySelector('#Spo').classList.remove('selected')
         document.querySelector('#Spk').classList.remove('selected')
         document.querySelector('#Abt').classList.remove('selected')
         break;
       default:
+        document.querySelector('#Sch').classList.remove('selected')
+        document.querySelector('#Sch').classList.remove('selected')
         document.querySelector('#Fq').classList.remove('selected')
         document.querySelector('#Spo').classList.remove('selected')
         document.querySelector('#Spk').classList.remove('selected')
